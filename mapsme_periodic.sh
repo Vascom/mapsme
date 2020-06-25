e1b6e48aed8fab6c703ba7af8e08a3db1867851c
vascom <vascom2@gmail.com>
2020-06-25 08:53:11 +0300

    Increase performance.

--- mapsme_periodic.sh
+++ mapsme_periodic.sh
@@ -35,6 +35,7 @@ done
 shift $((OPTIND-1))
 
 time_upload_server_path="vpnmercury:/home/vascom"
+CPU_THR=$(expr `grep -c processor /proc/cpuinfo` - 2)
 
 if [ $time_enable == "1" ]; then
     time_log_file="/tmp/mapsme_time.log"
@@ -44,6 +45,36 @@ if [ $time_enable == "1" ]; then
         rm $time_log_file
     fi
 fi
+
+# If Load average less then threshold or previously runned builds
+# not in Mwm state then wait to run next region
+function check_state() {
+    sleep 1m
+    state=0
+    while [ $state == "0" ]
+    do
+        #Check Load Average
+        load_average=$(cat /proc/loadavg | cut -d "." -f1)
+        while [ $load_average -gt $CPU_THR ]
+        do
+            sleep 1m
+            load_average=$(cat /proc/loadavg | cut -d "." -f1)
+        done
+
+        # Check maps build status
+        state=1
+        sleep 1m
+        states_list=$(find /mnt/ramdisk -name stages.status)
+        for i in $states_list
+        do
+            grep -q "Mwm" $i
+            if [ $? -ne 0 ]; then
+                state=0
+                break
+            fi
+        done
+    done
+}
 
 if [ $server_n == 0 ]; then
 
@@ -71,7 +102,8 @@ elif [ $server_n == 1 ]; then
     "Russia_Tver Oblast" \
     "Russia_Tula Oblast" \
     "Russia_Yaroslavl Oblast" &
-    sleep 10
+
+    check_state
 
     mapsme_gen.sh $GEN_ARGS -c 1 "russia" \
     "Russia_Bashkortostan" \
@@ -88,20 +120,21 @@ elif [ $server_n == 1 ]; then
     "Russia_Tatarstan" \
     "Russia_Udmurt Republic" \
     "Russia_Ulyanovsk Oblast" \
-    "Russia_Chuvashia"
+    "Russia_Chuvashia" &
 
-    wait
+    check_state
 
-    mapsme_gen.sh $GEN_ARGS -c 0 "russia" \
+    mapsme_gen.sh $GEN_ARGS -c 2 "russia" \
     "Russia_Krasnodar Krai" \
     "Russia_Krasnodar Krai_Adygeya" \
     "Russia_Astrakhan Oblast" \
     "Russia_Volgograd Oblast" \
     "Russia_Rostov Oblast" \
     "Russia_Republic of Kalmykia" &
-    sleep 10
 
-    mapsme_gen.sh $GEN_ARGS -c 1 "russia" \
+    check_state
+
+    mapsme_gen.sh $GEN_ARGS -c 3 "russia" \
     "Russia_Kurgan Oblast" \
     "Russia_Sverdlovsk Oblast_Ekaterinburg" \
     "Russia_Sverdlovsk Oblast_North" \
@@ -110,19 +143,20 @@ elif [ $server_n == 1 ]; then
     "Russia_Yugra_Surgut" \
     "Russia_Chelyabinsk Oblast" \
     "Russia_Yamalo-Nenets Autonomous Okrug" &
-    sleep 10
 
-    mapsme_gen.sh $GEN_ARGS -c 2 "belarus" \
+    check_state
+
+    mapsme_gen.sh $GEN_ARGS -c 4 "belarus" \
     "Belarus_Brest Region" \
     "Belarus_Homiel Region" \
     "Belarus_Hrodna Region" \
     "Belarus_Maglieu Region" \
     "Belarus_Minsk Region" \
-    "Belarus_Vitebsk Region"
+    "Belarus_Vitebsk Region" &
 
-    wait
+    check_state
 
-    mapsme_gen.sh $GEN_ARGS -c 0 "ukraine" \
+    mapsme_gen.sh $GEN_ARGS -c 5 "ukraine" \
     "Ukraine_Cherkasy Oblast" \
     "Ukraine_Chernihiv Oblast" \
     "Ukraine_Chernivtsi Oblast" \
@@ -147,9 +181,11 @@ elif [ $server_n == 1 ]; then
     "Ukraine_Zakarpattia Oblast" \
     "Ukraine_Zaporizhia Oblast" \
     "Ukraine_Zhytomyr Oblast" \
-    "Crimea"
+    "Crimea" &
 
-    mapsme_gen.sh $GEN_ARGS -c 0 "poland" \
+    check_state
+
+    mapsme_gen.sh $GEN_ARGS -c 6 "poland" \
     "Poland_Greater Poland Voivodeship" \
     "Poland_Kuyavian-Pomeranian Voivodeship" \
     "Poland_Lesser Poland Voivodeship" \
@@ -165,9 +201,71 @@ elif [ $server_n == 1 ]; then
     "Poland_Subcarpathian Voivodeship" \
     "Poland_Swietokrzyskie Voivodeship" \
     "Poland_Warmian-Masurian Voivodeship" \
-    "Poland_West Pomeranian Voivodeship"
+    "Poland_West Pomeranian Voivodeship" &
 
-    wait
+    check_state
+
+    mapsme_gen.sh $GEN_ARGS -c 7 "czech-republic" \
+    "Czech_Jihovychod_Jihomoravsky kraj" \
+    "Czech_Jihovychod_Kraj Vysocina" \
+    "Czech_Jihozapad_Jihocesky kraj" \
+    "Czech_Jihozapad_Plzensky kraj" \
+    "Czech_Karlovasky kraj" \
+    "Czech_Moravskoslezsko" \
+    "Czech_Olomoucky kraj" \
+    "Czech_Praha" \
+    "Czech_Severovychod_Kralovehradecky kraj" \
+    "Czech_Severovychod_Liberecky kraj" \
+    "Czech_Severovychod_Pardubicky kraj" \
+    "Czech_Stredni Cechy_East" \
+    "Czech_Stredni Cechy_West" \
+    "Czech_Ustecky kraj" \
+    "Czech_Zlinsky Kraj"&
+
+    check_state
+
+    mapsme_gen.sh $GEN_ARGS -c 8 "slovakia" \
+    "Slovakia_Region of Banska Bystrica" \
+    "Slovakia_Region of Bratislava" \
+    "Slovakia_Region of Kosice" \
+    "Slovakia_Region of Nitra" \
+    "Slovakia_Region of Presov" \
+    "Slovakia_Region of Trencin" \
+    "Slovakia_Region of Trnava" \
+    "Slovakia_Region of Zilina" &
+
+    check_state
+
+    mapsme_gen.sh $GEN_ARGS -c 9 "hungary" \
+    "Hungary_Kozep-Magyarorszag" \
+    "Hungary_Northern Great Plain" \
+    "Hungary_Transdanubia" &
+
+    check_state
+
+    mapsme_gen.sh $GEN_ARGS -c 10 "romania" \
+    "Romania_Centre" \
+    "Romania_North_East" \
+    "Romania_North_West" \
+    "Romania_South" \
+    "Romania_South_East" \
+    "Romania_South_West" \
+    "Romania_West" &
+
+    check_state
+
+    mapsme_gen.sh $GEN_ARGS -c 11 "bosnia-herzegovina" \
+    "Bosnia and Herzegovina_Brcko district of Bosnia and Herzegowina" \
+    "Bosnia and Herzegovina_Entity Federation of Bosnia and Herzegovina" \
+    "Bosnia and Herzegovina_Republic of Srpska" &
+
+    check_state
+
+    mapsme_gen.sh $GEN_ARGS -c 12 "slovenia" "Slovenia_East" "Slovenia_West" &
+
+    sleep 10
+
+    mapsme_gen.sh $GEN_ARGS -c 13 "serbia" "Serbia"
 
 elif [ $server_n == 2 ]; then
     mapsme_gen.sh $GEN_ARGS -c 0 "russia" \
@@ -184,7 +282,8 @@ elif [ $server_n == 2 ]; then
     "Russia_Novgorod Oblast" \
     "Russia_Pskov Oblast" \
     "Russia_Saint Petersburg" &
-    sleep 10
+
+    check_state
 
     mapsme_gen.sh $GEN_ARGS -c 1 "russia" \
     "Russia_Altai Krai" \
@@ -199,11 +298,11 @@ elif [ $server_n == 2 ]; then
     "Russia_Omsk Oblast" \
     "Russia_Tomsk Oblast" \
     "Russia_Tuva" \
-    "Russia_Khakassia"
+    "Russia_Khakassia" &
 
-    wait
+    check_state
 
-    mapsme_gen.sh $GEN_ARGS -c 0 "russia" \
+    mapsme_gen.sh $GEN_ARGS -c 2 "russia" \
     "Russia_Amur Oblast" \
     "Russia_Jewish Autonomous Oblast" \
     "Russia_Kamchatka Krai" \
@@ -213,52 +312,21 @@ elif [ $server_n == 2 ]; then
     "Russia_Sakhalin Oblast" \
     "Russia_Khabarovsk Krai" \
     "Russia_Chukotka Autonomous Okrug" &
-    sleep 10
 
-    mapsme_gen.sh $GEN_ARGS -c 1 "russia" \
+    check_state
+
+    mapsme_gen.sh $GEN_ARGS -c 3 "russia" \
     "Russia_Republic of Dagestan" \
     "Russia_Ingushetia" \
     "Russia_Kabardino-Balkaria" \
     "Russia_Karachay-Cherkessia" \
     "Russia_North Ossetia-Alania" \
     "Russia_Stavropol Krai" \
-    "Russia_Chechen Republic"
+    "Russia_Chechen Republic" &
 
-    wait
+    check_state
 
-    mapsme_gen.sh $GEN_ARGS -c 0 "russia" "Russia_Kaliningrad Oblast" &
-    sleep 10
-    mapsme_gen.sh $GEN_ARGS -c 1 "estonia" "Estonia_East" "Estonia_West" &
-    sleep 10
-    mapsme_gen.sh $GEN_ARGS -c 2 "latvia" "Latvia" &
-    sleep 10
-    mapsme_gen.sh $GEN_ARGS -c 3 "georgia" "Georgia" "Abkhazia" "South Ossetia" &
-    sleep 10
-    mapsme_gen.sh $GEN_ARGS -c 4 "armenia" "Armenia" &
-    sleep 10
-    mapsme_gen.sh $GEN_ARGS -c 5 "azerbaijan" "Azerbaijan" "Nagorno-Karabakh" &
-    sleep 10
-    mapsme_gen.sh $GEN_ARGS -c 6 "mongolia" "Mongolia" &
-    sleep 10
-    mapsme_gen.sh $GEN_ARGS -c 7 "north-korea" "North Korea" &
-    sleep 10
-    mapsme_gen.sh $GEN_ARGS -c 8 "turkmenistan" "Turkmenistan" &
-    sleep 10
-    mapsme_gen.sh $GEN_ARGS -c 9 "kyrgyzstan" "Kyrgyzstan" &
-    sleep 10
-    mapsme_gen.sh $GEN_ARGS -c 10 "tajikistan" "Tajikistan" &
-    sleep 10
-    mapsme_gen.sh $GEN_ARGS -c 11 "uzbekistan" "Uzbekistan" &
-
-    wait
-
-    mapsme_gen.sh $GEN_ARGS -c 0 "lithuania" "Lithuania_East" "Lithuania_West" &
-    sleep 10
-    mapsme_gen.sh $GEN_ARGS -c 1 "kazakhstan" "Kazakhstan_North" "Kazakhstan_South" &
-
-    wait
-
-    mapsme_gen.sh $GEN_ARGS -c 0 "finland" \
+    mapsme_gen.sh $GEN_ARGS -c 4 "finland" \
     "Finland_Eastern Finland_North" \
     "Finland_Eastern Finland_South" \
     "Finland_Northern Finland" \
@@ -266,62 +334,42 @@ elif [ $server_n == 2 ]; then
     "Finland_Southern Finland_Lappeenranta" \
     "Finland_Southern Finland_West" \
     "Finland_Western Finland_Jyvaskyla" \
-    "Finland_Western Finland_Tampere"
+    "Finland_Western Finland_Tampere" &
 
-elif [ $server_n == 3 ]; then
-    mapsme_gen.sh $GEN_ARGS -c 0 "czech-republic" \
-    "Czech_Jihovychod_Jihomoravsky kraj" \
-    "Czech_Jihovychod_Kraj Vysocina" \
-    "Czech_Jihozapad_Jihocesky kraj" \
-    "Czech_Jihozapad_Plzensky kraj" \
-    "Czech_Karlovasky kraj" \
-    "Czech_Moravskoslezsko" \
-    "Czech_Olomoucky kraj" \
-    "Czech_Praha" \
-    "Czech_Severovychod_Kralovehradecky kraj" \
-    "Czech_Severovychod_Liberecky kraj" \
-    "Czech_Severovychod_Pardubicky kraj" \
-    "Czech_Stredni Cechy_East" \
-    "Czech_Stredni Cechy_West" \
-    "Czech_Ustecky kraj" \
-    "Czech_Zlinsky Kraj"
+    check_state
 
-   mapsme_gen.sh $GEN_ARGS -c 0 "serbia" "Serbia" &
-   sleep 10
-   mapsme_gen.sh $GEN_ARGS -c 1 "slovakia" \
-   "Slovakia_Region of Banska Bystrica" \
-   "Slovakia_Region of Bratislava" \
-   "Slovakia_Region of Kosice" \
-   "Slovakia_Region of Nitra" \
-   "Slovakia_Region of Presov" \
-   "Slovakia_Region of Trencin" \
-   "Slovakia_Region of Trnava" \
-   "Slovakia_Region of Zilina" &
-   sleep 10
-   mapsme_gen.sh $GEN_ARGS -c 2 "slovenia" "Slovenia_East" "Slovenia_West" &
+    mapsme_gen.sh $GEN_ARGS -c 5 "lithuania" "Lithuania_East" "Lithuania_West" &
 
-   wait
+    sleep 10
 
-   mapsme_gen.sh $GEN_ARGS -c 0 "hungary" \
-   "Hungary_Kozep-Magyarorszag" \
-   "Hungary_Northern Great Plain" \
-   "Hungary_Transdanubia" &
-   sleep 10
-   mapsme_gen.sh $GEN_ARGS -c 1 "romania" \
-   "Romania_Centre" \
-   "Romania_North_East" \
-   "Romania_North_West" \
-   "Romania_South" \
-   "Romania_South_East" \
-   "Romania_South_West" \
-   "Romania_West" &
-   sleep 10
-   mapsme_gen.sh $GEN_ARGS -c 2 "bosnia-herzegovina" \
-   "Bosnia and Herzegovina_Brcko district of Bosnia and Herzegowina" \
-   "Bosnia and Herzegovina_Entity Federation of Bosnia and Herzegovina" \
-   "Bosnia and Herzegovina_Republic of Srpska" &
+    mapsme_gen.sh $GEN_ARGS -c 6 "kazakhstan" "Kazakhstan_North" "Kazakhstan_South" &
 
-   wait
+    check_state
+
+    mapsme_gen.sh $GEN_ARGS -c 7 "russia" "Russia_Kaliningrad Oblast" &
+    sleep 10
+    mapsme_gen.sh $GEN_ARGS -c 8 "estonia" "Estonia_East" "Estonia_West" &
+    sleep 10
+    mapsme_gen.sh $GEN_ARGS -c 9 "latvia" "Latvia" &
+    sleep 10
+    mapsme_gen.sh $GEN_ARGS -c 10 "georgia" "Georgia" "Abkhazia" "South Ossetia" &
+    sleep 10
+    mapsme_gen.sh $GEN_ARGS -c 11 "armenia" "Armenia" &
+    sleep 10
+    mapsme_gen.sh $GEN_ARGS -c 12 "azerbaijan" "Azerbaijan" "Nagorno-Karabakh" &
+    sleep 10
+    mapsme_gen.sh $GEN_ARGS -c 13 "mongolia" "Mongolia" &
+    sleep 10
+    mapsme_gen.sh $GEN_ARGS -c 14 "north-korea" "North Korea" &
+    sleep 10
+    mapsme_gen.sh $GEN_ARGS -c 15 "turkmenistan" "Turkmenistan" &
+    sleep 10
+    mapsme_gen.sh $GEN_ARGS -c 16 "kyrgyzstan" "Kyrgyzstan" &
+    sleep 10
+    mapsme_gen.sh $GEN_ARGS -c 17 "tajikistan" "Tajikistan" &
+    sleep 10
+    mapsme_gen.sh $GEN_ARGS -c 18 "uzbekistan" "Uzbekistan"
+
 elif [ $server_n == "2d" ]; then
     mapsme_gen.sh $GEN_ARGS -c 0 -p "russia" \
     "Russia_Moscow" \
